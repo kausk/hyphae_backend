b6762163b08420adbe70f1be422bd73fb3bbd86b
Kaushik Kasi <ksk@Kaushiks-MacBook-Pro.local>
2019-07-28 19:07:38 -0700

    cleaned up commented code

--- routes/image.js
+++ routes/image.js
@@ -21,8 +21,6 @@ const savedImgLocation = '/public/output.png';
 const savedFileFolderPath = '/Users/ksk/hyphae_backend/processed/';
 var router = express.Router();
 
-
-
 // Referenced from https://medium.com/@mahesh_joshi/reactjs-nodejs-upload-image-how-to-upload-image-using-reactjs-and-nodejs-multer-918dc66d304c
 const storage = multer.diskStorage({
   destination: function(req, file, cb){
@@ -59,80 +57,11 @@ router.post('/', upload.single('image'), (req, res, next) => {
         area: area,
         result_img_location: filename
       });
-        
-      /*
-      const upload_url = uploadFileS3(filename);
-      if (upload_url != -1) {
-        console.log('successful');
-        console.log(upload_url);
-        res.json({
-          area: area,
-          result_img_location: upload_url
-        });
-      } else {
-        console.log('error');
-        res.status(500);
-      }
-      
-      return uploadFileS3(filename)
-        .then(result => {
-          console.log('result img location');
-          console.log(result);
-
-
-        })
-        .catch(error => {
-          console.log('error');
-          res.status(500);
-      });
-      */
 
     });
   
     // Delete created file and saved file
 });
 
-/*
-router.get('/', function(req, res, next) {
-  res.render('image', { title: 'Express' });
-});
-
-
-// Referenced from https://medium.com/@mahesh_joshi/reactjs-nodejs-upload-image-how-to-upload-image-using-reactjs-and-nodejs-multer-918dc66d304c
-// Referenced https://flaviocopes.com/express-forms-files/
-router.post('/', upload.any(), function(req, res, next) {
-
-  if (req.files.length == 0) {
-    res.status(400);
-    res.render('Must upload an image');
-  }
-
-
-  
-  // Referenced https://github.com/richardgirges/express-fileupload/tree/master/example
-
-  let filename = req.files[0].filename
-  let options = {
-    args: [filename]
-  };
-
-  let result;
-  PythonShell.run('hyphae_detector_modified.py', options, function (err, results) {
-    if (err) throw err;
-    console.log(results);
-    result = results;
-    console.log(savedFilePath);
-    // res.sendFile(savedFilePath);
-    res.json({
-      area: result[0],
-      location: savedFilePath // saved image location
-    });
-  });
-
-  //
-  // Renaming code - doable
-
-});
-*/
 
 module.exports = router;
